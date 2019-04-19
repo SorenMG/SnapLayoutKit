e0c07a458143e9a34aded4b3a6822979e3a5adef
Soren <soren1146@gmail.com>
2019-04-19 20:18:27 +0200

    update example

--- iOS-Example/ViewController.swift
+++ iOS-Example/ViewController.swift
@@ -42,16 +42,12 @@ class ViewController: UIViewController {
             .safe()
             .snap(to: view)
         
-//        print("Green: ", greenView.snapConstraints)
-        
         view.addSubview(redView)
         redView
             .exec()
             .centerY(to: greenView)
             .snapRight(view, offset: -100)
             .size(.init(width: 100, height: 100))
-        
-//        print("Red: ", redView.snapConstraints)
 
         view.addSubview(blueView)
         blueView
@@ -60,17 +56,8 @@ class ViewController: UIViewController {
             .size(.init(width: 50, height: 50))
         
         blueView
-//            .delete(.centerX)
-//            .delete(.centerY)
-//            .snapLeft(view, offset: 0)
-//            .snapTop(view, offset: 0)
-        .update(constraint: .centerX, toConstraint: .left, on: greenView, offset: 0)
-        .update(constraint: .centerY, toConstraint: .top, on: greenView, offset: 0)
-        
-//        print("Blue: ", blueView.snapConstraints)
-//        blueView.update(constraint: .width, toConstraint: .none, on: view, offset: 100)
-//        blueView.update(constraint: .centerX, toConstraint: .left, on: greenView, offset: 0)
-//        blueView.update(constraint: .centerY, toConstraint: <#T##UIView.Attribute#>, on: <#T##UIView#>, offset: <#T##CGFloat#>)
+            .update(constraint: .centerX, toConstraint: .left, on: greenView, offset: 0)
+            .update(constraint: .centerY, toConstraint: .top, on: greenView, offset: 0)
     }
 }
 
